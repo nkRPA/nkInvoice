5cf7e57da23d1720cea7515a101a28222c7a706e
larskoldkasper <lakas@naestved.dk>
2025-12-04 11:08:23 +0100

    changed enum types for all enums

--- sample.py
+++ sample.py
@@ -11,7 +11,19 @@ import asyncio
 
 load_dotenv()
 
+import json
+from enum import Enum
+from datetime import date, datetime
+from decimal import Decimal
 
+def json_serializer(obj):
+    if isinstance(obj, Enum):
+        return obj.name      # or obj.value
+    if isinstance(obj, (date, datetime)):
+        return obj.isoformat()
+    if isinstance(obj, Decimal):
+        return float(obj)
+    return str(obj)  # fallback
 
 
 async def main():
@@ -56,7 +68,7 @@ async def main():
     cost_data_list = [
             {
                 "Artskonto":"40000000",
-                "PSP_element":"XG-0000000204-00001",
+                "PSP_element":"XG-0000003295-00008",
                 "Kost":cost1,
                 "PosteringsTekst":"Test postering 1",
                 "Type":eOpusCostType.DEBET
@@ -79,27 +91,54 @@ async def main():
             "BilagsFilePath":bilags_file_path,
             "csv_filename":csv_filename
         }
-    retries = 1
-        # Try multiple times to find the correct iframe and attach the file
-    for attempt in range(retries):
-        try:
-            print(f"** Create Invoice attempt {attempt + 1} of {retries}")
-            logger.info(f"** Create Invoice attempt {attempt + 1} of {retries}")
-            # Create an instance of nkInvoice
-            invoice = nkInvoice(opus_data=opus_data, invoice_data=invoice_data)
-            # Set headless and verbose mode
-            invoice._headless=False
-            invoice._verbose=True
-            ## Set logger if needed
-            invoice._logger=logging.getLogger(__name__)
-            ## Create the invoice
-            result = await invoice.create_invoice(max_retries=3, try_number=1) 
-            # result is a dictionary with the result of the operation
-            # eks. {"status": "success", "message": "Invoice created successfully", "bilag": "123456"}
-            logger.info(f"Result: {result}")
-        except Exception as e:
-            print(f"Error: {e}")
-            logger.error(f"Error: {e}")
+    
+      
+    print(f"invoice_data: {json.dumps(invoice_data, indent=4, default=json_serializer)}")
+    
+    
+    
+    invoice_data = {
+        "Tekst": "Test af tekst",
+        "opus_cost_data": [
+            {
+                "Artskonto": "40000000",
+                "PSP_element": "XG-0000003295-00008",
+                "Kost": 1.0,
+                "PosteringsTekst": "Test postering 1",
+                "Type": "DEBET"
+            },
+            {
+                "Artskonto": "40000000",
+                "PSP_element": "XG-0000002473-00029",
+                "Kost": 1.0,
+                "PosteringsTekst": "Test postering 1",
+                "Type": "KREDIT"
+            }
+        ],
+        "Reference": "test af ref",
+        "Bogføringsdato": "04.12.2025",
+        "Kommentar": "test af comment",
+        "BilagsFilePath": "/Users/lakas/tmp/file_path.txt",
+        "csv_filename": "/Users/lakas/tmp/opus.csv"
+    }
+    
+    
+    try:
+        # Create an instance of nkInvoice
+        invoice = nkInvoice(opus_data=opus_data, invoice_data=invoice_data)
+        # Set headless and verbose mode
+        invoice._headless=False
+        invoice._verbose=True
+        ## Set logger if needed
+        invoice._logger=logging.getLogger(__name__)
+        ## Create the invoice
+        result = await invoice.create_invoice(max_retries=3, try_number=1) 
+        # result is a dictionary with the result of the operation
+        # eks. {"status": "success", "message": "Invoice created successfully", "bilag": "123456"}
+        logger.info(f"Result: {result}")
+    except Exception as e:
+        print(f"Error: {e}")
+        logger.error(f"Error: {e}")
 
 
     # send_internal_email(receiver_email, subject, body, smtp_server='smtp.naestved.dk', smtp_port=2552, sender_email="InvoiceTester@naestved.dk", attachments=[log_filename])
